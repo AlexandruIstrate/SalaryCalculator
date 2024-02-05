2098241051a10dd875c82d107615d531ed7dc969
Alexandru Istrate <38143225+AlexandruIstrate@users.noreply.github.com>
2024-02-05 22:58:08 +0200

    Dynamically generate footer content

--- salary-calc/src/components/FooterContent.jsx
+++ salary-calc/src/components/FooterContent.jsx
@@ -5,6 +5,53 @@ import { withTranslation } from "react-i18next";
 import NewTabLink from "src/components/NewTabLink";
 
 function FooterContent({ t }) {
+    // Create an object to hold all the footer links
+    const footerLinks = {
+        sections: {
+            docs: {
+                i18nKey: "footer.docs.title",
+                links: {
+                    pppDocs: {
+                        iconName: "bi-question-circle",
+                        i18nKey: "footer.docs.whatIsPPP",
+                        link: "https://en.wikipedia.org/wiki/Purchasing_power_parity"
+                    },
+                    worldBankAPI: {
+                        iconName: "bi-puzzle",
+                        i18nKey: "footer.docs.worldBankAPI",
+                        link: "https://documents.worldbank.org/en/publication/documents-reports/api"
+                    }
+                }
+            },
+            project: {
+                i18nKey: "footer.project.title",
+                links: {
+                    projHomepage: {
+                        iconName: "bi-github",
+                        i18nKey: "footer.project.homepage",
+                        link: "https://github.com/AlexandruIstrate/SalaryCalculator"
+                    },
+                    docs: {
+                        iconName: "bi-book",
+                        i18nKey: "footer.project.docs",
+                        link: "https://github.com/AlexandruIstrate/SalaryCalculator/blob/master/README.md"
+                    },
+                    reportIssue: {
+                        iconName: "bi-flag",
+                        i18nKey: "footer.project.reportIssue",
+                        link: "https://github.com/AlexandruIstrate/SalaryCalculator/issues"
+                    },
+                    prevReleases: {
+                        iconName: "bi-box-seam",
+                        i18nKey: "footer.project.previousReleases",
+                        link: "https://github.com/AlexandruIstrate/SalaryCalculator/releases"
+                    }
+                }
+            }
+        }
+    };
+
+    // Return the HTML
     return (
         <Container className="text-center text-md-left">
             <div className="row">
@@ -15,59 +62,27 @@ function FooterContent({ t }) {
 
                 <hr className="clearfix w-100 d-md-none pb-0" />
 
-                <div className="col-md-3 mb-md-0 mb-3">
-                    <h5 className="text-uppercase">{t("footer.docs.title")}</h5>
-                    <ul className="list-unstyled">
-                        <li>
-                            <i className="bi bi-question-circle me-1" />&nbsp;
-                            <NewTabLink
-                                href="https://en.wikipedia.org/wiki/Purchasing_power_parity"
-                                title={t("footer.docs.whatIsPPP")}
-                            />
-                        </li>
-                        <li>
-                            <i className="bi bi-puzzle me-1" />&nbsp;
-                            <NewTabLink
-                                href="https://documents.worldbank.org/en/publication/documents-reports/api"
-                                title={t("footer.docs.worldBankAPI")}
-                            />
-                        </li>
-                    </ul>
-                </div>
-
-                <div className="col-md-3 mb-md-0 mb-3">
-                    <h5 className="text-uppercase">{t("footer.project.title")}</h5>
-                    <ul className="list-unstyled">
-                        <li>
-                            <i className="bi bi-github me-1" />&nbsp;
-                            <NewTabLink
-                                href="https://github.com/AlexandruIstrate/SalaryCalculator"
-                                title={t("footer.project.homepage")}
-                            />
-                        </li>
-                        <li>
-                            <i className="bi bi-book me-1" />&nbsp;
-                            <NewTabLink
-                                href="https://github.com/AlexandruIstrate/SalaryCalculator/blob/master/README.md"
-                                title={t("footer.project.docs")}
-                            />
-                        </li>
-                        <li>
-                            <i className="bi bi-flag me-1" />&nbsp;
-                            <NewTabLink
-                                href="https://github.com/AlexandruIstrate/SalaryCalculator/issues"
-                                title={t("footer.project.reportIssue")}
-                            />
-                        </li>
-                        <li>
-                            <i className="bi bi-box-seam me-1" />&nbsp;
-                            <NewTabLink
-                                href="https://github.com/AlexandruIstrate/SalaryCalculator/releases"
-                                title={t("footer.project.previousReleases")}
-                            />
-                        </li>
-                    </ul>
-                </div>
+                {/* Dinamically render all the links */}
+                {
+                    Object.values(footerLinks.sections).map(({ i18nKey, links }) => (
+                        <div className="col-md-3 mb-md-0 mb-3">
+                            <h5 className="text-uppercase">{t(i18nKey)}</h5>
+                            <ul className="list-unstyled">
+                                {
+                                    Object.values(links).map(({ iconName, i18nKey, link }, index) => (
+                                        <li key={index}>
+                                            <i className={`bi ${iconName} me-1`} />&nbsp;
+                                            <NewTabLink
+                                                href={link}
+                                                title={t(i18nKey)}
+                                            />
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+                        </div>
+                    ))
+                }
             </div>
         </Container>
     )
