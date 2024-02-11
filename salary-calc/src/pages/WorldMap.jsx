cb8b70778090f42828dca26cf7b926f64ba654a1
Alexandru Istrate <38143225+AlexandruIstrate@users.noreply.github.com>
2024-02-11 15:55:00 +0200

    Add WorldMap page

---
+++ salary-calc/src/pages/WorldMap.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function WorldMap() {
+    return (
+        <div className="calculator">
+            <h1>World Map</h1>
+        </div>
+    )
+}
+
+export default WorldMap;
